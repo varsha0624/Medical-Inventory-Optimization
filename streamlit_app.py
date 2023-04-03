b5132f09872f5d6ff4c2e9dbd18464052fceb6d6
varsha0624 <106689283+varsha0624@users.noreply.github.com>
2023-04-03 19:28:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import pickle
 from datetime import datetime
 
 # Load the model
-with open(r"F:\Model Depolyment\model.pickle",'rb') as f:
+with open(r"F:\Model Depolyment\model.pickle",'wb') as f:
     model = pickle.load(f)
 
 
