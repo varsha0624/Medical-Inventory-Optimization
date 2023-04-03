cd69132a7a2b9fc282c98fa93b28746c061eba09
varsha0624 <106689283+varsha0624@users.noreply.github.com>
2023-04-03 18:52:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from PIL import Image
 import warnings
 warnings.filterwarnings("ignore")
