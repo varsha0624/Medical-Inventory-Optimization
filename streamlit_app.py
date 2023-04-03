ae950bd18b8e6fa69a000a03ccb6034560557050
varsha0624 <106689283+varsha0624@users.noreply.github.com>
2023-04-03 17:47:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,15 +9,7 @@ try:
     with open(r"C:\Users\Varsh\model.pickle", 'rb') as f:
         model = pickle.load(f)
  
-# Define a function to take user input and make prediction
-def predict_quantity(date):
-    # Convert date string to datetime object
-    date = datetime.strptime(date, '%Y-%m-%d')
-    # Create a pandas Timestamp object
-    date = pd.Timestamp(date)
-    # Make a prediction using the loaded model
-    prediction = model.predict(start=date, end=date)[0]
-    return prediction
+
 
 # Create a Streamlit app
 st.set_page_config(page_title='Medicine Demand Forecasting App', page_icon=':pill:', layout='wide', initial_sidebar_state='collapsed')
